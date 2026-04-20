6be8a256b7b6f35fe5c0cc339d5b65ea763e7a9d
yuto yamada <y_yamada@craid-inc.com>
2026-04-20 19:02:01 +0900

    編集/削除の実装

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'db_connect.php';
+
+// URLパラメータから削除したいIDを取得
+$id = $_GET['id'] ?? null;
+
+if ($id) {
+    // 指定したIDのデータを削除するSQL
+    // WHERE句を忘れると全データが消える
+    $sql = 'DELETE FROM tasks WHERE id = ?';
+
+    // クラス化した $db の query メソッドを使って安全に削除
+    $db->query($sql, [$id]);
+}
+
+// 削除が終わったら（あるいはIDがなくても）一覧画面に戻る
+header('Location: index.php');
+exit;
